cf69333dd72f4586fa5b869909c4e8dbe8c87f71
Tom Huynh <tomhuynhsg@gmail.com>
2022-11-22 14:15:35 +0700

    Fix package path issue

--- src/tutorialsolution5/exercise2/Restaurant.java
+++ src/tutorialsolution5/exercise2/Restaurant.java
@@ -1,16 +1,14 @@
 package tutorialsolution5.exercise2;
 
-import tutorialsolution5.exercise3.Ingredient;
-
 public class Restaurant {
     public static void main(String[] args) {
         // exercise 2
         // create first ingredient object
-        tutorialsolution5.exercise3.Ingredient i1 = tutorialsolution5.exercise3.Ingredient.createIngredient();
+        Ingredient i1 = Ingredient.createIngredient();
         i1.displayIngredient();
 
         // create second ingredient object
-        tutorialsolution5.exercise3.Ingredient i2 = Ingredient.createIngredient();
+        Ingredient i2 = Ingredient.createIngredient();
         i2.displayIngredient();
     }
 
