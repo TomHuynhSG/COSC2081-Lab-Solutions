cc8eb3808bb72988e8910d07a4982f90beff9d44
Tom Huynh <tomhuynhsg@gmail.com>
2022-12-09 11:48:53 +0700

    add a method to calculate volumn of Cylinder

--- src/tutorialsolution7/exercise1/Main.java
+++ src/tutorialsolution7/exercise1/Main.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter the radius of the circle:");
         double radius = scanner.nextDouble();
+
         Circle myCircle = new Circle(radius);
+
         System.out.println("The area of the circle: "+ myCircle.getArea());
         System.out.println("The circle info:" + myCircle);
 
         System.out.println("Enter the height of the cylinder:");
         double height = scanner.nextDouble();
+
         Cylinder myCylinder = new Cylinder(radius, height);
+
         System.out.println("The area of the cylinder: "+ myCylinder.getArea());
         System.out.println("The volumn of the cylinder: "+ myCylinder.getVolumn());
         System.out.println("The cylinder info:" + myCylinder);
--- src/tutorialsolution7/exercise2/Main.java
+++ src/tutorialsolution7/exercise2/Main.java
@@ -9,18 +9,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter the color:");
         String color = scanner.nextLine();
+
         System.out.println("Enter the length and width of the rectangle:");
         int length = scanner.nextInt();
         int width = scanner.nextInt();
+
+        // Create a Rectangle object with given color, length and width from the user
         Rectangle myRectangle = new Rectangle(color, length, width);
+
         System.out.println("The area of the rectangle: "+ myRectangle.getArea());
         System.out.println("The rectangle info:" + myRectangle);
 
         System.out.println("Enter the base and height of the triangle:");
         int base = scanner.nextInt();
         int height = scanner.nextInt();
+
+        // Create a Triangle object with given color, base and height from the user
         Triangle myTriangle = new Triangle(color, base, height);
         System.out.println("The area of the triangle: "+ myTriangle.getArea());
         System.out.println("The triangle info:" + myTriangle);
