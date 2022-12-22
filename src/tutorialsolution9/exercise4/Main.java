dbce2dc2165864264176888c7a2ff74970d59290
tomhuynh <tomhuynhsg@gmail.com>
2022-12-22 13:31:25 +0700

    Add sample solutions for week 9 tutorial!

--- src/tutorialsolution9/exercise4/Main.java
+++ src/tutorialsolution9/exercise4/Main.java
@@ -2,6 +2,7 @@ package tutorialsolution9.exercise4;
 
 import java.util.Calendar;
 import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,10 +11,10 @@ public class Main {
         Employee e1 = new Employee("E101","Jerry",500,1,4);
         Employee e2 = new Employee("E102","Tom",200,2,3);
         Employee e3 = new Employee("E103","Gary",300,1,2);
-        Employee e4 = new Employee("E104","Jess",200,1,5);
-        Employee e5 = new Employee("E105","Sinh",600,1,1);
-        Employee e6 = new Employee("E106","Khanh",800,4,3);
-        Employee e7 = new Employee("E107","Tu",300,1,3);
+        Employee e4 = new Employee("E104","Mary",200,1,5);
+        Employee e5 = new Employee("E105","Phong",600,1,1);
+        Employee e6 = new Employee("E106","Christina",800,4,3);
+        Employee e7 = new Employee("E107","Josh",300,1,3);
         myProject.addEmployee(e1);
         myProject.addEmployee(e2);
         myProject.addEmployee(e3);
